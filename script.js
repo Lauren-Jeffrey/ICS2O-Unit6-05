42ed1dfae3f50f1e17f25e13ecf2b1c8e241c459
LaurenJeffrey <lauren.jeffrey@ocsbstudent.ca>
2021-01-06 15:33:48 +0000

    js edited

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 document.getElementById('determine').addEventListener('click', myfunction)
 function myfunction () {
-  let day = document.getElementById('whatday').value
-  let age = document.getElementById('whatage').value
-  
-  if ((day != 'Saturday' && day != 'Sunday') && (age < 18)) {
-    alert ('You have school today')
+  const day = document.getElementById('whatday').value
+  const age = document.getElementById('whatage').value
+  if ((day !== 'Saturday' && day !== 'Sunday') && (age < 18)) {
+    alert('You have school today')
   }
-  if ((day != 'Saturday' && day != 'Sunday') && (age > 18)) {
-    alert ('You have work today!');
-  }
-  else if (day == 'Saturday' || day == 'Sunday') 
-    alert ('Relax for the weekend!')
+  if ((day !== 'Saturday' && day !== 'Sunday') && (age > 18)) {
+    alert('You have work today!')}
+  else if (day == 'Saturday' || day == 'Sunday') {
+    alert('Relax for the weekend!')}
 }
